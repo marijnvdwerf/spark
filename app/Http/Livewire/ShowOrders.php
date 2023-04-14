bc7d239e88f0360aa1885ebdbee3e6f77ee1f6a9
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2023-04-14 12:24:55 +0200

    Add week-based pagination

--- app/Http/Livewire/ShowOrders.php
+++ app/Http/Livewire/ShowOrders.php
@@ -2,13 +2,71 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Location;
 use App\Models\Order;
+use Carbon\Carbon;
 use Livewire\Component;
 
 class ShowOrders extends Component
 {
+    public $location;
+
+    // TODO: refactor this to share less data with the client.
+    public $weeks;
+
+    public $week;
+
+    public function mount()
+    {
+        $this->weeks = $this->getWeeks();
+        $this->week = count($this->weeks) - 1; // Select most recent week
+    }
+
+    private function getWeeks()
+    {
+        // TODO: refactor and look into making database agnostic
+        $days = Order::select('created_at')
+            ->groupByRaw('DATE(created_at)')
+            ->get()
+            ->map(fn(Order $order) => $order->created_at)
+            ->sort();
+
+        $weeks = [];
+
+        /** @var Carbon $lastDay */
+        $lastDay = $days->last();
+
+        /** @var Carbon $startOfWeek */
+        $startOfWeek = $days->first()->copy()->startOfWeek();
+        while (true) {
+            if ($startOfWeek > $lastDay) {
+                break;
+            }
+
+            $weeks[] = $startOfWeek->format('o-W');
+            $startOfWeek = $startOfWeek->addWeek();
+        }
+
+        return $weeks;
+    }
+
     public function render()
     {
-        return view('livewire.show-orders', ['orders' => Order::paginate(10)]);
+        $query = Order::query();
+        if ($this->location) {
+            $query->where('location_id', $this->location);
+        }
+
+        // Carbon doesn't like year-week, see https://stackoverflow.com/a/52062781
+        list($year, $week) = explode('-', $this->weeks[$this->week]);
+        $weekStart = Carbon::create()
+            ->setISODate($year, $week)  // TODO: verify how this works when day-year and week-year differ
+            ->startOfWeek();
+        $weekEnd = $weekStart->copy()->endOfWeek();
+        $orders = $query->get()
+            ->filter(function (Order $order) use ($weekEnd, $weekStart) {
+                return $order->created_at->between($weekStart, $weekEnd);
+            });
+        return view('livewire.show-orders', ['orders' => $orders, 'locations' => Location::all()]);
     }
 }
--- resources/views/livewire/show-orders.blade.php
+++ resources/views/livewire/show-orders.blade.php
@@ -1,4 +1,11 @@
 <div>
+    <input type="range" wire:model="week" min="0" max="{{ count($weeks) - 1 }}" value="{{$week}}">
+    <select wire:model="location">
+        <option value="0">Nederland</option>
+        @foreach($locations as $location)
+            <option value="{{ $location->id }}">{{ $location->name }}</option>
+        @endforeach
+    </select>
     <table>
         <thead>
         <tr>
@@ -24,6 +31,4 @@
         @endforeach
         </tbody>
     </table>
-
-    {{ $orders->links() }}
 </div>
